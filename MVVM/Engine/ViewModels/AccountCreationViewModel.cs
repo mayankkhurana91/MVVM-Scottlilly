b6b07795ec681b1b5120e81fef19c1eca6fd0a26
Mayank Khurana <mayank.khurana@alpine.com>
2018-11-26 15:22:59 +0100

    first commit

---
+++ MVVM/Engine/ViewModels/AccountCreationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    public class AccountCreationViewModel
+    {
+        public AccountModel NewAccount { get; set; }
+
+        public AccountCreationViewModel()
+        {
+            NewAccount = new AccountModel();
+        }
+
+        public void ValidatePassword()
+        {
+            NewAccount.ValidatePassword();
+        }
+    }
+}
---
+++ MVVM/MainApplication/AccountCreationView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Engine.ViewModels;
+
+namespace MainApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AccountCreationView : Window
+    {
+        private readonly AccountCreationViewModel _viewModel = new AccountCreationViewModel();
+
+        public AccountCreationView()
+        {
+            InitializeComponent();
+            DataContext = _viewModel;
+        }
+
+        private void OnClick_ValidatePassword(object sender, RoutedEventArgs e)
+        {
+            _viewModel.ValidatePassword();
+        }
+    }
+}
